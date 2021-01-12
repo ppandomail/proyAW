bf66c1cc5e188f50efd35f04336b660a5eb51446
ppando <ppando@192.168.1.19>
2021-01-11 21:15:12 -0300

    main

---
+++ proyAW/src/main/java/com/proy_aw/proyAW/ProyAwApplication.java
@@ -0,0 +1,13 @@
+package com.proy_aw.proyAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyAwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyAwApplication.class, args);
+	}
+
+}
